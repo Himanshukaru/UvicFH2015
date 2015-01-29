922246766bb37324c27f26e58025a3bd46d15123
Marc Laventure <marclave@uvic.ca>
2015-01-29 10:52:38 -0800

    Create MainApplication.py
    Basic structure of the Main application, by no means working just a layout

---
+++ infotainment/src/MainApplication.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import ttk
+import math
+import time
+
+from modules import FuelGauge, ChargeGauge, Speedometer
+
+class MainApplication(object):
+	"""
+	This is the MainApplication class that will instantiate
+	all other "widgets" and where, hopefully, the canbus data is being handled
+	"""
+	
+	
+if __name__ == "__main__":
+	mainApp = MainApplication()
