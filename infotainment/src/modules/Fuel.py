f821c5049a131d6aa90713b977ecb54555dc3086
marclave <laventure.marc@gmail.com>
2015-02-12 20:15:10 -0800

    Fuel is still in testing phase, works but needs to be modified

---
+++ infotainment/src/modules/Fuel.py
@@ -0,0 +1,61 @@
+from tkinter import *
+from tkinter import ttk
+import math
+import time
+
+class Speedometer(object):
+    
+    root = Tk()
+    def __init__(self):        
+        self.initializeValues()
+        self.makeMainFrame()
+        self.makeCanvas()
+        
+        self.updateFuel()
+        self.root.mainloop()
+
+    def initializeValues(self):
+        self.backGroundColor = "#ffffff"
+        self.outlineColor = "black"
+        self.backGroundFillColor = "white"
+        self.fillColor = "black"
+        
+        self.frame_rate = 80
+        self.center = 110
+        self.radius = 110
+        self.pieAngle = 90
+        
+        self.piePositionAngle = 310 # TODO m: need to do this dynamically -> 2*x + 100 = 180 marc you know math
+        self.tempCounter = 0 #TODO m: this will be the charge count; for now its a temp counter
+
+    def makeMainFrame(self):
+        self.mainframe = ttk.Frame(self.root, padding="0 0 0 0")
+        self.mainframe.grid(column=1, row=0, columnspan=1, rowspan=2, sticky=(N, E, W, S))
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
+
+    def makeCanvas(self):
+        self.chargePie = Canvas(self.mainframe, width=250, height=250, background=self.backGroundColor)
+        self.chargePie.grid(column=0, row=1, sticky=(N,E,W,S))
+        self.chargePie.create_arc(self.center-self.radius, self.center-self.radius, self.center+self.radius, self.center+self.radius, style=PIESLICE, extent=self.pieAngle, outline=self.outlineColor, fill=self.backGroundFillColor, start=self.piePositionAngle)
+        
+        self.chargeDeltaPie = self.chargePie.create_arc(self.center-self.radius, self.center-self.radius, self.center+self.radius, self.center+self.radius, style=PIESLICE, extent=self.pieAngle, outline=self.outlineColor, fill=self.fillColor, start=self.piePositionAngle)
+
+    def updateFuel(self):
+        try:
+            self.tempCounter += 1
+
+            if (self.tempCounter > self.pieAngle):
+                self.tempCounter = self.pieAngle
+            if (self.tempCounter < 0):
+                self.tempCounter = 0
+                            
+            self.chargePie.itemconfigure(self.chargeDeltaPie, extent=self.tempCounter)
+            self.chargePie.update()
+        except ValueError:
+            pass
+        self.root.after(self.frame_rate,self.updateFuel)
+        
+if __name__ == "__main__":
+    
+    testClass = Speedometer()
